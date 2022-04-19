54c9b5b96d78ef26da8a2d91e96b496e235affee
matt <matthieu.l.dubray@gmail.com>
2022-04-19 11:43:22 -0700

    basic flask app, with user and post model defined

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from models import db, flask_bcrypt
+import environ
+import os
+
+env = environ.Env(DEBUG=(bool, False))
+basedir = os.path.abspath(os.path.dirname(__file__))
+environ.Env.read_env(os.path.join(basedir, '.env'))
 
 app = Flask(__name__)
 
-api = Api(app)
+# config options
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = env('SECRET_KEY')
+app.config['PORT'] = 8080
+app.config['DEBUG'] = True
 
+db.init_app(app)
+flask_bcrypt.init_app(app)
+
+app.app_context().push()
+
+db.create_all()
 
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,60 @@
+from datetime import datetime as dt
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+flask_bcrypt = Bcrypt()
+
+
+class PostModel(db.Model):
+    __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    imageUrl = db.Column(db.String(255), unique=True)
+    body = db.Column(db.Text(), nullable=False)
+    slug = db.Column(db.String(255))
+
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author = db.relationship('User', back_populates='posts')
+
+    createdAt = db.Column(db.DateTime, nullable=False)
+    updatedAt = db.Column(db.DateTime, nullable=False)
+
+    def json(self):
+        return {
+            "id": self.id,
+            "title": self.title
+        }
+
+
+class UserModel(db.Model):
+    """ User Model for storing user related details """
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    password_hash = db.Column(db.String(100))
+    createdAt = db.Column(db.DateTime, nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
+
+    def json(self):
+        return {
+            "name": self.first_name + ' ' + self.last_name,
+            "id": self.id,
+            "username": self.username
+                }
