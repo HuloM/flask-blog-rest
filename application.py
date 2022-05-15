4ccf0bbac9f331416f2f3f6559c40d052c800b9f
matt <matthieu.l.dubray@gmail.com>
2022-05-15 01:49:56 -0700

    gunicorn requires renaming app variable to application

--- application.py
+++ application.py
@@ -14,60 +14,60 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # reads variables from the .env file
 environ.Env.read_env(os.path.join(basedir, '.env'))
 
-app = Flask(__name__, static_url_path='',
-			static_folder='uploads')
-CORS(app, resources=r'/*')
+application = Flask(__name__, static_url_path='',
+					static_folder='uploads')
+CORS(application, resources=r'/*')
 # config options
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config['DEBUG'] = True
-app.config['ENV'] = 'development'
-app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'uploads\\images')
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+application.config['DEBUG'] = True
+application.config['ENV'] = 'development'
+application.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'uploads\\images')
 
-db.init_app(app)
-flask_bcrypt.init_app(app)
+db.init_app(application)
+flask_bcrypt.init_app(application)
 
-app.app_context().push()
-migrate = Migrate(app, db)
+application.app_context().push()
+migrate = Migrate(application, db)
 db.create_all()
 
 
-@app.route('/static/<path:path>')
+@application.route('/static/<path:path>')
 def send_report(path):
-	img_path = os.path.join(app.config['UPLOAD_FOLDER'], str(path))
+	img_path = os.path.join(application.config['UPLOAD_FOLDER'], str(path))
 	return send_file(img_path)
 
 
-@app.route('/signup', methods=['PUT'])
+@application.route('/signup', methods=['PUT'])
 def signup():
 	return user_signup()
 
 
-@app.route('/login', methods=['POST'])
+@application.route('/login', methods=['POST'])
 def login():
 	return user_login()
 
 
-@app.route('/post/<int:index>/', methods=['GET', 'PUT', 'DELETE'])
+@application.route('/post/<int:index>/', methods=['GET', 'PUT', 'DELETE'])
 def post(index):
 	return RUD_post(index)
 
 
-@app.route('/post/comments/<int:index>', methods=['POST'])
+@application.route('/post/comments/<int:index>', methods=['POST'])
 def comment(index):
 	return C_comments(index)
 
 
-@app.route('/posts', methods=['POST'])
+@application.route('/posts', methods=['POST'])
 def CreatePost():
 	return C_posts()
 
 
-@app.route('/posts/<int:page>/', defaults={'page': 1})
+@application.route('/posts/<int:page>/', defaults={'page': 1})
 def posts(page):
 	return retrieve_all_posts(page)
 
 
 if __name__ == '__main__':
-	app.run(port=8080)
+	application.run(port=8080)
